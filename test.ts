2f6b5db0b7debdac3c519e8c73cc135eb4269b0d
Yakiyo <yakiyosama5855@gmail.com>
2023-02-12 14:31:11 +0000

    feat: more tests

--- test.ts
+++ test.ts
@@ -1,10 +1,17 @@
-import { assertEquals } from 'https://deno.land/std@0.177.0/testing/asserts.ts';
-import { Result } from './mod.ts';
+import { assert } from 'https://deno.land/std@0.177.0/testing/asserts.ts';
+import { Err, Ok, Result } from './mod.ts';
 
-Deno.test({
-	name: 'Initialization test',
-	fn() {
-		const r = Result.from('h');
-		assertEquals(true, r instanceof Result);
-	},
+Deno.test('Initialization test', () => {
+	const r = Result.from(() => 'Test');
+	assert(r instanceof Result);
+});
+
+Deno.test('Ok test', () => {
+	const result = Ok('Test');
+	assert(result.is_ok());
+});
+
+Deno.test('Err test', () => {
+	const result = Err('Error');
+	assert(result.is_err());
 });
