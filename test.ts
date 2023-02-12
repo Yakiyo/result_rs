fa9c74e505ce5685e02e9701a958ebcf65a5c4bd
Yakiyo <yakiyosama5855@gmail.com>
2023-02-12 12:01:54 +0000

    chore: deno fmt

--- test.ts
+++ test.ts
@@ -1,10 +1,10 @@
-import { assertEquals } from "https://deno.land/std@0.177.0/testing/asserts.ts";
-import { Result } from "./mod.ts";
+import { assertEquals } from 'https://deno.land/std@0.177.0/testing/asserts.ts';
+import { Result } from './mod.ts';
 
 Deno.test({
-    name: 'Initialization test',
-    fn() {
-        const r = Result.from('h');
-        assertEquals(true ,r instanceof Result)
-    }
-})
+	name: 'Initialization test',
+	fn() {
+		const r = Result.from('h');
+		assertEquals(true, r instanceof Result);
+	},
+});
