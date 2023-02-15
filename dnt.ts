da4b7f70f77e99f4f0596707705ee6913c9d39ca
Yakiyo <yakiyosama5855@gmail.com>
2023-02-15 19:33:58 +0600

    feat: use pnpm as package manager

--- dnt.ts
+++ dnt.ts
@@ -14,6 +14,7 @@ await emptyDir('./npm');
 await build({
 	entryPoints: ['./mod.ts'],
 	outDir: './npm',
+	packageManager: 'pnpm',
 	shims: {
 		// see JS docs for overview and more options
 		deno: true,
