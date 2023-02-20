6fac66c6bad13e22717e358c19f55f1e68350a18
Yakiyo <yakiyosama5855@gmail.com>
2023-02-20 11:00:02 +0000

    feat: Option constructors and Some, None funcs

--- src/option.ts
+++ src/option.ts
@@ -1,5 +1,26 @@
-export class Option {}
+export class Option<T> {
+	private value: T | null;
 
-export function Some() {}
+	private constructor(v?: T) {
+		if (v) this.value = v;
+		else this.value = null;
+	}
 
-export const None = new Option();
+	private static isFunction(
+		x: unknown,
+	): x is (...args: readonly any[]) => any {
+		return typeof x === 'function';
+	}
+	public static from<T>(f?: T | (() => T)) {
+		if (this.isFunction(f)) {
+			return new Option(f());
+		}
+		return new Option(f);
+	}
+}
+
+export function Some<T>(input: T) {
+	return Option.from(input);
+}
+
+export const None = Option.from<never>();
